a8572e10b872c928a4ec68ed558d1c5ee8752237
damien <dalien2611@hotmail.fr>
2014-04-19 01:37:49 -0400

    on off working

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,33 +1,22 @@
-int led = 13;
+int led = 52;
 int incomingByte = 0;
 
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(led, OUTPUT);   
   Serial.begin(9600);  
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
 
+  if (Serial.available() > 0) {
+      incomingByte = 0;
+      incomingByte = Serial.read();
 
+      if(incomingByte == 48){ // 48 means 1
+        digitalWrite(led, HIGH );
+      }else { // else write low
+       digitalWrite(led, LOW); 
+      }
+  }
+}
 
-        if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
-
-                // say what you got:
-                Serial.print("I received: "+ incomingByte );
-//                Serial.println(incomingByte, DEC);
-                
-                if(incomingByte == 49){
-                  digitalWrite(led, LOW); 
-                }else{
-                 digitalWrite(led, HIGH); 
-
-                }
-
-        }
-      
-    }
